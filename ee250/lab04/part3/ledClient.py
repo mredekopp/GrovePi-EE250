3e8b165766e1735180059f48509cc5a946dff0b7
Mark Redekopp <redekopp@usc.edu>
2018-05-31 12:36:40 -0700

    updated

--- ee250/lab04/part3/ledClient.py
+++ ee250/lab04/part3/ledClient.py
@@ -2,3 +2,30 @@
 #
 # This code sends requests to the Raspberry Pi to turn on and
 #off the Grove LED using TCP packets.
+import socket
+
+def Main():
+    """127.0.0.1 is the loopback address. Any packets sent to this address will
+    essentially loop right back to your machine and look for any process 
+    listening in on the port specified."""
+    # host = '10.1.2.15'
+    host = '192.168.1.124'
+    port = 9002
+
+    c = socket.socket() #by default, the socket constructor creates an TCP/IPv4 socket
+    c.connect((host,port))
+
+    message = input("-> ")
+    while message != 'q':
+        c.send(message.encode('utf-8')) 
+        #1024 is the receive buffer size. It's enough for us, and it's a nice number. 
+        data = c.recv(1024).decode('utf-8') 
+        print("Status: " + data)
+        message = input("-> ")
+    c.close()
+
+"""This if-statement checks if you are running this python file directly. That 
+is, if you run `python3 tcpClient.py` in terminal, this if-statement will be 
+true"""
+if __name__ == '__main__':
+    Main()
--- ee250/lab04/part3/ledServer.py
+++ ee250/lab04/part3/ledServer.py
@@ -9,5 +9,66 @@ import sys
 sys.path.append('../../../Software/Python/')
 
 import grovepi
+import time
+from grovepi import *
 
 # use TCP
+"""Example python3 server-side socket program using TCP from the tutorial video below.
+
+https://www.youtube.com/watch?v=bTThyxVy7Sk&index=6&list=PL1A2CSdiySGIPxpSlgzsZiWDavYTAx61d
+"""
+import socket
+
+def Main():
+    # Connect the Grove LED to digital port D4
+    led = 4
+
+    pinMode(led,"OUTPUT")
+    time.sleep(1)
+
+#    host = '10.0.2.15' #'127.0.0.1'
+    host = '192.168.1.213'
+    port = 9001
+
+    s = socket.socket()
+    s.bind((host,port))
+
+    s.listen(1)
+    c, addr = s.accept()
+    print("Connection from: " + str(addr))
+    while True:
+        try:
+            data = c.recv(1024).decode('utf-8')
+            if not data:
+                break
+
+            if(data == 'LED_ON'):
+                digitalWrite(led,1)		# Send HIGH to switch on LED
+                print ("LED ON!")
+                c.send("LED turned ON".encode('utf-8'))
+            elif(data == 'LED_OFF'):
+                digitalWrite(led,0)		# Send LOW to switch off LED
+                print ("LED OFF!")
+                c.send("LED turned OFF".encode('utf-8'))
+            elif(data == 'q'):
+                print("Received quit command...exiting!")
+                c.send("Server shutdown!".encode('utf-8'))
+                break
+            else:
+                c.send("Unrecognized command".encode('utf-8'))
+        except KeyboardInterrupt:	# Turn LED off before stopping
+            digitalWrite(led,0)		# Send LOW to switch off LED
+            c.send("Server had an unexpected error...quitting".encode('utf-8'))
+            break
+        except IOError:				# Print "Error" if communication error encountered
+            c.send("Server had an unexpected error...quitting".encode('utf-8'))
+            print ("Error")
+            break
+
+    c.close()
+
+
+
+        
+if __name__ == '__main__':
+    Main()
